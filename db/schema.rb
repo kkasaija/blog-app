acdd3d9460a78dd0e1c510640904e54e4263f87b
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-26 22:47:44 +0300

    Add user-comments, post-comments foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_194041) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_194523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_194041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "AuthorId"
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
