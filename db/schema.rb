04ce291c59027b9a8d54ac85148e2b54af6c0bb1
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-26 16:06:59 +0300

    Rename comments table columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_123048) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_123049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "post_id"
-    t.text "text"
+    t.integer "PostId"
+    t.text "Text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id"
+    t.integer "AuthorId"
   end
 
   create_table "likes", force: :cascade do |t|
