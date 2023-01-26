59abd5048d7369ff78fdd8490c473c1a0a76cc9e
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-26 15:56:07 +0300

    Rename posts table columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_110851) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_123043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,13 +30,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_110851) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.string "Title"
+    t.text "Text"
+    t.integer "CommentsCounter"
+    t.integer "LikesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id"
+    t.integer "AuthorId"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,5 +47,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_110851) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
