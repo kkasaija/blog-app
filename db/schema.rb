9f89146c0957489dcce0fdc9f8eb26c5f5d8fd1f
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-26 14:02:23 +0300

    Add author_id column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_193149) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_105957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_193149) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
   end
 
   create_table "users", force: :cascade do |t|
