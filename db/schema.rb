90ef720757d20e8aaccdd8b7756d947d9c762316
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-26 22:38:42 +0300

    Add user-posts foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_123050) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_193541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_123050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "AuthorId"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,36 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 first_user = User.create(
-  name: 'Tom',
-  photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-  bio: 'Teacher from Mexico.'
+  Name: 'Tom',
+  Photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+  Bio: 'Teacher from Mexico.'
 )
 second_user = User.create(
-  name: 'Lilly',
-  photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-  bio: 'Teacher from Poland.'
+  Name: 'Lilly',
+  Photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+  Bio: 'Teacher from Poland.'
+)
+
+first_post = Post.create(
+  Author: first_user,
+  Title: 'Hello',
+  Text: 'This is my first post'
+)
+
+second_post = Post.create(
+  Author: first_user,
+  Title: 'Welcome',
+  text: 'This is my second post'
+)
+
+third_post = Post.create(
+  Author: first_user,
+  Title: 'Praise and Warship',
+  Text: 'This is my third post'
+)
+
+fourth_post = Post.create(
+  Author: first_user,
+  Title: 'Resting',
+  Text: 'This is my fourth post'
 )
