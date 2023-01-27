8efa65cb21e6f8954b86a59607db38cbd88e6f2d
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-27 20:40:49 +0300

    Update user_spec tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,16 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.') }
-  before { subject.save }
+  before(:each) do
+    @new_user = User.create(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.')
+  end
 
   it 'name should be present' do
-    subject.Name = nil
-    expect(subject).to_not be_valid
+    @new_user.Name = nil
+    expect(@new_user).to_not be_valid
   end
 
   it 'bio should be present' do
-    subject.Bio = nil
-    expect(subject).to_not be_valid
+    @new_user.Bio = nil
+    expect(@new_user).to_not be_valid
+  end
+
+  it 'PostsCounter should be present' do
+    @new_user.PostsCounter = nil
+    expect(@new_user).to_not be_valid
+  end
+
+  it 'PostsCounter should allow valid values' do
+    @new_user.PostsCounter = 20
+    expect(@new_user).to be_valid
+  end
+  
+  it 'Name should be present' do
+    @new_user.Name = nil
+    expect(@new_user).to_not be_valid
   end
 end
