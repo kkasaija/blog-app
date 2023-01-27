fef5717a57cc8fb9b7175beba2b5cde596e01f8d
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-27 13:51:21 +0300

    Add model validation for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,18 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :Title, presence: true, length: { maximum: 250 }
+  
+  validates :CommentsCounter, numericality: {
+    greater_than_or_equal_to: 0,
+    only_integer: true
+  }
+
+  validates :LikesCounter, numericality: {
+    greater_than_or_equal_to: 0,
+    only_integer: true
+  }
+
   after_save :update_post_counter
 
   def update_post_counter
