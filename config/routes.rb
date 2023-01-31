250a8000966c43d4b6174d463c1f919c2d7898c6
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-31 16:17:36 +0300

    Add routes to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
   get '/users/:id', to: 'users#show'
+  get 'users/:id/posts', to:'posts#index'
+  get 'users/:id/posts/:id', to:'posts#show'
 end
