28f643f608e120de3bc9c53a508a3321ba1322a7
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-03 19:13:14 +0300

    Update post controller actions to create and save new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,35 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:author_id])
+    @posts = Post.where(author_id: @user)
+  end
+
+  def new
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
+    end
+  end
+
+  def create
+    @post = Post.new(post_parameters)
+    @post.user = current_user
+    @post.author_id = current_user.id
+
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new
+    end
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
-  def new
-    @post = Post.new
+  private
+
+  def post_parameters
+    params.require(:new_post).permit(:Title, :Text)
   end
 end
