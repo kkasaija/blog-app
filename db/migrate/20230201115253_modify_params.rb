776abebd166f839b49cda11d3344bcdf55426968
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-07 19:51:41 +0300

    Update schema to comply with Ruby best practices

--- db/migrate/20230201115253_modify_params.rb
+++ db/migrate/20230201115253_modify_params.rb
@@ -1,10 +1,10 @@
 class ModifyParams < ActiveRecord::Migration[7.0]
   def change
-    remove_column :comments, :PostId, :integer
-    remove_column :comments, :AuthorId, :integer
-    remove_column :likes, :PostId, :integer
-    remove_column :likes, :AuthorId, :integer
-    remove_column :posts, :AuthorId, :integer
+    remove_column :comments, :post_id, :integer
+    remove_column :comments, :author_id, :integer
+    remove_column :likes, :post_id, :integer
+    remove_column :likes, :author_id, :integer
+    remove_column :posts, :author_id, :integer
     rename_column :comments, :user_id, :author_id
     rename_column :likes, :user_id, :author_id
     rename_column :posts, :user_id, :author_id
