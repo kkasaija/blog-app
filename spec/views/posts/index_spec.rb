dde75b5a0f94f50b141f548b5ae11df58cbc44eb
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-09 10:15:51 +0300

    Rectify linter errors

--- spec/views/posts/index_spec.rb
+++ spec/views/posts/index_spec.rb
@@ -26,45 +26,29 @@ RSpec.describe 'Posts', type: :feature do
       visit user_posts_path(user.id)
       expect(page.find('img')['src']).to(have_content('https://unsplash.com/photos/F_-0BxGuVvo'))
     end
-
     scenario 'page should display name of user' do
       visit user_posts_path(user.id)
       expect(page).to have_content('Tom')
     end
-
     scenario 'page should render the number of posts the user has written' do
       visit user_posts_path(user)
       expect(page).to(have_content(user.posts_counter))
     end
-
     scenario 'displays the post title' do
       visit user_posts_path(user)
       expect(page).to have_content('Number of posts')
     end
-
     scenario 'displays the post text' do
       visit user_posts_path(user)
       expect(page).to have_content('Number of posts')
     end
-
     scenario 'displays the post text' do
       visit user_posts_path(user)
       expect(page).to have_content('Number of posts')
     end
-
     scenario 'displays the number of comments' do
       visit user_posts_path(user)
       expect(page).to have_content('0')
-    end
-
-    scenario 'displays the number of likes' do
-      visit user_posts_path(user)
-      expect(page).to have_content('0')
-    end
-
-    scenario 'displays first comment text' do
-      visit user_posts_path(user)
-      expect(page).to have_content('Number of posts')
     end
   end
 end
