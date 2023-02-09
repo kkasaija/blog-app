ae6b950f6a850170bc7f186126fbd61cb634692d
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-09 16:03:30 +0300

    Update users index test

--- spec/views/users/index_spec.rb
+++ spec/views/users/index_spec.rb
@@ -1,40 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :system do
-  before(:all) do
-    @user1 = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                         bio: 'Teacher from Mexico.', posts_counter: 0)
-
-    @user2 = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                         bio: 'Teacher from Poland.')
-
-    @post = Post.create(user: @user1, title: 'One', text: 'This is my first post')
-    Comment.create(author_id: @user1.id, post_id: @post.id, text: "Tom's second comment")
-  end
-
-  describe 'index page' do
-    it 'should render names of users' do
+RSpec.describe 'Users', type: :feature do
+  describe 'User index page process' do
+    before(:each) do
+      @user = User.create(
+        name: 'Kisembo',
+        photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+        bio: 'Teacher from Mexico.',
+        posts_counter: 0
+      )
       visit users_path
-      expect(page).to have_content('Tom')
-      expect(page).to have_content('Lilly')
     end
 
-    it 'should display the picture in users profile' do
-      visit users_path
-      images = page.all('img')
-      expect(images[0]['src']).to have_content('https://unsplash.com/photos/F_-0BxGuVvo')
-      expect(images[1]['src']).to have_content('https://unsplash.com/photos/F_-0BxGuVvo')
+    it 'should show the username of all other users' do
+      expect(page).to have_content(@user.name)
     end
 
-    it 'should display the number of posts of each user' do
-      visit users_path
-      expect(page).to have_content('0')
+    it 'should show the photo of all other users' do
+      expect(page.body).to include(@user.photo)
     end
 
-    it 'after clicking on a user, the user profile page is displayed' do
-      visit users_path
-      click_link @user1.name
-      expect(page).to have_current_path user_path(@user1)
+    it 'should show the number of posts of all other users' do
+      expect(page.body).to include(@user.posts_counter.to_s)
+    end
+
+    it "When I click on a user, should redirected to that user's show page." do
+      click_link(@user.name)
+      expect(page).to have_current_path(user_path(@user.id))
     end
   end
 end
