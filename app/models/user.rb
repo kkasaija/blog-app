c57f808c886950e82a6d3135f544152f4180f3a9
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-10 18:11:01 +0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # attr_accessor :email_confirmation
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :Confirmable
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
+  # validates :email, confirmation: true
   after_initialize :set_defaults
   validates :posts_counter, numericality: {
     greater_than_or_equal_to: 0,
